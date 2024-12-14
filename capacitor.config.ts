f299b190a0085d5b0a83b1a8454532eeb638781c
Perard Alison <alison.perard.grans@gmail.com>
2024-12-14 16:09:46 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'smartOrderClient',
+  webDir: 'www'
+};
+
+export default config;
