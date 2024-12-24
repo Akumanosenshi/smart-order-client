fcc134ae76609e7784f6ad56ea40903a598a426c
Akumanosenshi <alison.perard.grans@gmail.com>
2024-12-24 02:28:56 +0100

    start

---
+++ README.md
@@ -0,0 +1,9 @@
+Generer un composant ou une page
+ionic g
+
+NE JAMAIS IMPORTER UN COMPONENT DAN LE -routing.module.ts
+
+chercher integration d router pour rediriger les pages sur clic d un element
+
+img ds asset 
+
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)
   }
+
 ];
 @NgModule({
   imports: [
